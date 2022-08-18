a82d77ab67094c74189063e28ce9ad0e4e3c5515
Guilherme Samuel <guisamuel800@gmail.com>
2022-08-18 16:21:29 -0300

    feat: all colors into a class file

---
+++ lib/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class ProjectColors {
+    final title = Color(0xFF000000);
+    final textField = Color(0xffF5F6FA);
+    final circleBackground = Color(0xffBDBDBD);
+}
