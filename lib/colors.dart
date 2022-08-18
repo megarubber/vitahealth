bd36867c3757b98c971accb6369b5f95e9453655
Guilherme Samuel <guisamuel800@gmail.com>
2022-08-18 17:19:18 -0300

    feat: add button colors

--- lib/colors.dart
+++ lib/colors.dart
@@ -4,4 +4,6 @@ class ProjectColors {
     final title = Color(0xFF000000);
     final textField = Color(0xffF5F6FA);
     final circleBackground = Color(0xffBDBDBD);
+    final buttonBackground = Color(0xff00BCD4);
+    final buttonText = Color(0xFFFFFFFF);
 }
