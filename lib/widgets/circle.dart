155b456aa0e1dc506fd6af7a347b92942b3fe162
Guilherme Samuel <guisamuel800@gmail.com>
2022-08-18 16:51:25 -0300

    feat: circle class file

---
+++ lib/widgets/circle.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:vitahealth/colors.dart';
+
+class Circle {
+  Widget createCircle({required double diameter, required double x, required double y}) {
+    return Container(
+      width: diameter,
+      height: diameter,
+      decoration: new BoxDecoration(
+        color: ProjectColors().circleBackground,
+        shape: BoxShape.circle,
+      ),
+      transform: Matrix4.translationValues(
+      x, y, 0.0),
+    );
+  }
+}
