686d06d6046519aa9eb628a48112648c145cf821
Guilherme Samuel <guisamuel800@gmail.com>
2022-08-24 15:40:24 -0300

    feat: textField colors

--- lib/colors.dart
+++ lib/colors.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 class ProjectColors {
     final title = Color(0xFF000000);
     final textField = Color(0xffF5F6FA);
+    final textFieldBlock = Color(0xff666666);
+    final textFieldWrong = Color(0xffFFBDBD);
     final circleBackground = Color(0xffBDBDBD);
     final buttonBackground = Color(0xff00BCD4);
     final buttonText = Color(0xFFFFFFFF);
