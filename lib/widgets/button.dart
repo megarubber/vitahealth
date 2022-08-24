2366f18a0230a1c5455e5264a2754788cf68695b
Guilherme Samuel <guisamuel800@gmail.com>
2022-08-24 15:50:29 -0300

    feat: Login Button

--- lib/widgets/button.dart
+++ lib/widgets/button.dart
@@ -2,11 +2,30 @@ import 'package:flutter/material.dart';
 import 'package:vitahealth/colors.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 class Button {
+  void pressAction() {
+    print("You pressed the button!");
+  }
+
+  void showToast(String message) {
+    Fluttertoast.showToast(
+      msg: message,
+      toastLength: Toast.LENGTH_SHORT,
+      gravity: ToastGravity.BOTTOM,
+      timeInSecForIosWeb: 1,
+      backgroundColor: Colors.black,
+      textColor: Colors.white,
+      fontSize: 16.0,
+    );
+  }
+
   Widget createButton({required String message}) {
     return OutlinedButton(
-      onPressed: () {},
+      onPressed: () {
+        pressAction();
+      },
       style: ButtonStyle(
         shape: MaterialStateProperty.all(RoundedRectangleBorder(borderRadius: BorderRadius.circular(30.0))),
         backgroundColor: MaterialStateProperty.all(ProjectColors().buttonBackground),
@@ -20,5 +39,23 @@ class Button {
       ),
       child: Text(message)
     );
+  }
+}
+
+class LoginButton extends Button {
+  int countButtonPressed = 0;
+
+  @override
+  void pressAction() {
+    countButtonPressed++;
+    if(countButtonPressed < 3) {
+      showToast("Usuário/Senha inválidos!");
+    } else {
+      showToast("Login bloqueado: aguarde 30s!");    
+    }
+  }
+
+  int getCounter() {
+    return countButtonPressed;
   }
 }
