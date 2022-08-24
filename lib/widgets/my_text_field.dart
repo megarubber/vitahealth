88d9d688ae382b7180868390b5edb3cc19b08b9c
Guilherme Samuel <guisamuel800@gmail.com>
2022-08-24 15:51:01 -0300

    feat: changing color of TextInput (login)

--- lib/widgets/my_text_field.dart
+++ lib/widgets/my_text_field.dart
@@ -4,26 +4,40 @@ import 'package:vitahealth/colors.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class MyTextField {
-  Widget createTextField({required String hint}) {
+  Color changeColor([int mode = 0]) {
+    switch(mode) {
+      case 0:
+        return ProjectColors().textField;
+      case 1:
+        return ProjectColors().textFieldBlock;
+      case 2:
+        return ProjectColors().textFieldWrong;
+      default:
+        return ProjectColors().textField;
+    }
+  }
+
+  Widget createTextField({required String hint, int mode = 0}) {
     return TextField(
       decoration: InputDecoration(
-          border: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(8.r),
-            borderSide: BorderSide(color: Colors.transparent, width: 0),
-          ),
-          enabledBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(8.r),
-            borderSide: BorderSide(color: Colors.transparent, width: 0),
-          ),
-          contentPadding:
-              EdgeInsets.symmetric(horizontal: 16.w, vertical: 14.h),
-          filled: true,
-          fillColor: ProjectColors().textField,
-          hintText: hint,
-          hintStyle: GoogleFonts.poppins(
-            fontSize: 14.sp,
-            fontWeight: FontWeight.w400,
-          )),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(8.r),
+          borderSide: BorderSide(color: Colors.transparent, width: 0),
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(8.r),
+          borderSide: BorderSide(color: Colors.transparent, width: 0),
+        ),
+        contentPadding:
+            EdgeInsets.symmetric(horizontal: 16.w, vertical: 14.h),
+        filled: true,
+        fillColor: changeColor(mode),
+        hintText: hint,
+        hintStyle: GoogleFonts.poppins(
+          fontSize: 14.sp,
+          fontWeight: FontWeight.w400,
+        )
+      ),
     );
   }
 }
