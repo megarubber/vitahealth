88f73bb8ccf5b583f70e388640dbbc0b94bd9abe
Guilherme Samuel <guisamuel800@gmail.com>
2022-08-25 17:21:26 -0300

    feat: config top part (appBar & image picker)

--- lib/screens/register.dart
+++ lib/screens/register.dart
@@ -1,11 +1,63 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:vitahealth/widgets/my_text_field.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:vitahealth/widgets/button.dart';
+import 'package:vitahealth/colors.dart';
+import 'dart:io';
 
-class Register extends StatelessWidget {
+class Register extends StatefulWidget {
+  @override
+  RegisterState createState() => RegisterState();
+}
+
+class RegisterState extends State<Register> {
+  File? profileImage;
+
+  Future<void> pickImage() async {
+    final image = await ImagePicker().pickImage(source: ImageSource.gallery);
+    if(image == null) return;
+    final imageTemporary = File(image.path);
+    setState(() => profileImage = imageTemporary);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("eae")
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(
+            Icons.arrow_back_ios,
+          ),
+          onPressed: () {
+            
+          }
+        ),
+        title: Text("Cadastro de Usuários"),
+        backgroundColor: ProjectColors().appBarBackground,
+        foregroundColor: ProjectColors().appBarForeground,
+        elevation: 0
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 24.w),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(height: 40.h),
+              InkWell(
+                onTap: () => pickImage(), 
+                child: Center(
+                  child: Image.asset(
+                    'assets/images/user_icon.png',
+                    width: 100.sp,
+                    height: 100.sp
+                  ),
+                )
+              )
+            ]
+          )
+        )
       )
     );
   }
