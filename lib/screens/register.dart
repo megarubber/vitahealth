723b541ef2a2697e6572fde832dfbfeda1dd0a9b
Guilherme Samuel <guisamuel800@gmail.com>
2022-08-26 14:39:36 -0300

    feat: form textfields

--- lib/screens/register.dart
+++ lib/screens/register.dart
@@ -5,6 +5,7 @@ import 'package:image_picker/image_picker.dart';
 import 'package:vitahealth/widgets/button.dart';
 import 'package:vitahealth/colors.dart';
 import 'dart:io';
+import 'package:vitahealth/widgets/my_text_field.dart';
 
 class Register extends StatefulWidget {
   @override
@@ -13,6 +14,7 @@ class Register extends StatefulWidget {
 
 class RegisterState extends State<Register> {
   File? profileImage;
+  final spaceBetween = 16;
 
   Future<void> pickImage() async {
     final image = await ImagePicker().pickImage(source: ImageSource.gallery);
@@ -41,11 +43,11 @@ class RegisterState extends State<Register> {
       ),
       body: SafeArea(
         child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 24.w),
+          padding: EdgeInsets.symmetric(horizontal: 15.w),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              SizedBox(height: 40.h),
+              SizedBox(height: 10.h),
               InkWell(
                 onTap: () => pickImage(), 
                 child: Center(
@@ -55,7 +57,91 @@ class RegisterState extends State<Register> {
                     height: 100.sp
                   ),
                 )
-              )
+              ),
+              SizedBox(height: spaceBetween.h),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Icon(
+                    Icons.account_circle_rounded,
+                    size: 50.sp,
+                  ),
+                  SizedBox(
+                    width: 270.w,
+                    child: MyTextField().createTextField(hint: "Nome e Sobrenome")
+                  )
+                ]
+              ),
+              SizedBox(height: spaceBetween.h),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Icon(
+                    Icons.email,
+                    size: 50.sp,
+                  ),
+                  SizedBox(
+                    width: 270.w,
+                    child: MyTextField().createTextField(hint: "Email")
+                  )
+                ]
+              ),
+              SizedBox(height: spaceBetween.h),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Icon(
+                    Icons.local_phone_rounded,
+                    size: 50.sp,
+                  ),
+                  SizedBox(
+                    width: 270.w,
+                    child: MyTextField().createTextField(hint: "Telefone")
+                  )
+                ]
+              ),
+              SizedBox(height: spaceBetween.h),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Icon(
+                    Icons.star,
+                    size: 50.sp,
+                  ),
+                  SizedBox(
+                    width: 270.w,
+                    child: MyTextField().createTextField(hint: "Nome de usuário")
+                  )
+                ]
+              ),
+              SizedBox(height: spaceBetween.h),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Icon(
+                    Icons.key,
+                    size: 50.sp,
+                  ),
+                  SizedBox(
+                    width: 270.w,
+                    child: MyTextField().createTextField(hint: "Senha")
+                  )
+                ]
+              ),
+              SizedBox(height: spaceBetween.h),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Icon(
+                    Icons.lock,
+                    size: 50.sp,
+                  ),
+                  SizedBox(
+                    width: 270.w,
+                    child: MyTextField().createTextField(hint: "Confirme a senha")
+                  )
+                ]
+              ),
             ]
           )
         )
