866570b5328d33ae98ae7b396c42474c706f8aed
Guilherme Samuel <guisamuel800@gmail.com>
2022-09-08 15:44:44 -0300

    feat: setup register profile

---
+++ lib/screens/register_profile.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:vitahealth/colors.dart';
+import 'package:vitahealth/widgets/my_text_field.dart';
+
+class RegisterProfile extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 15.w),
+          child: Center(
+            child: Column(
+              children: <Widget>[
+                Text(
+                  'Olá, Usuário!',
+                  style: GoogleFonts.poppins(
+                    fontSize: 25.sp,
+                    color: ProjectColors().title
+                  )
+                ),
+                SizedBox(height: 20.h),
+                Text(
+                  'Seja Bem Vindo!',
+                  style: GoogleFonts.poppins(
+                    fontSize: 25.sp,
+                    color: ProjectColors().title
+                  )
+                ),
+                SizedBox(height: 20.h),
+                Text(
+                  'Como esse é seu primeiro acesso, precisamos '
+                  'de mais algumas informações para continuar!',
+                  style: GoogleFonts.poppins(
+                    fontSize: 15.sp,
+                    color: ProjectColors().title
+                  ),
+                  textAlign: TextAlign.center
+                ),
+              ]
+            )
+          )
+        )
+      )
+    );
+  }
+}
