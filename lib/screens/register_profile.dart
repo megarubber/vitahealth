a1159209e894441609229d55ce23805ff24468ca
Guilherme Samuel <guisamuel800@gmail.com>
2022-09-13 15:15:51 -0300

    feat: calculates BMI

--- lib/screens/register_profile.dart
+++ lib/screens/register_profile.dart
@@ -5,8 +5,34 @@ import 'package:vitahealth/colors.dart';
 import 'package:vitahealth/widgets/my_text_field.dart';
 import 'package:intl/intl.dart';
 
-class RegisterProfile extends StatelessWidget {
+class RegisterProfile extends StatefulWidget {
+  @override
+  RegisterProfileStatus createState() => RegisterProfileStatus();
+}
+
+class RegisterProfileStatus extends State<RegisterProfile> {
   TextEditingController dateinput = TextEditingController();
+
+  String bmiStatus = 'Coloque seus dados para calcular seu IMC!';
+
+  void updateBMIStatus(double height, double mass) {
+    double result = (mass/(height * height));
+    
+    String baseMsg = 'Seu IMC é de: ' + result.toString();
+
+    if(result < 18.5)
+      baseMsg += ', o que indica MAGREZA!';
+    else if(result >= 18.5 && result < 25.0)
+      baseMsg += ', o que indica NORMAL!';
+    else if(result >= 25.0 && result < 30.0)
+      baseMsg += ', o que indica SOBREPESO!';
+    else if(result >= 30.0 && result < 40.0)
+      baseMsg += ', o que indica OBESIDADE!';
+    else
+      baseMsg += ', o que indica OBESIDADE GRAVE!';
+    
+    bmiStatus = baseMsg;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -54,16 +80,25 @@ class RegisterProfile extends StatelessWidget {
                   children: <Widget>[
                     SizedBox(
                       width: 160.w,
-                      child: MyTextField().createNumberField(hint: 'Peso')
+                      child: MyTextField(changedValue: setState() => updateBMIStatus(10.0, 10.0)).createNumberField(hint: 'Peso (kg)')
                     ),
                     SizedBox(
                       width: 10.w
                     ),
                     SizedBox(
                       width: 160.w,
-                      child: MyTextField().createNumberField(hint: 'Altura')
+                      child: MyTextField(changedValue: setState() => updateBMIStatus(10.0, 10.0)).createNumberField(hint: 'Altura (m)')
                     ),
                   ]
+                ),
+                SizedBox(height: 20.h),
+                Text(
+                  bmiStatus,
+                  style: GoogleFonts.poppins(
+                    fontSize: 15.sp,
+                    color: ProjectColors().title
+                  ),
+                  textAlign: TextAlign.center
                 )
               ]
             )
