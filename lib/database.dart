1f536182c0aca835a5c56b09dd226149a35c1e03
Guilherme Samuel <guisamuel800@gmail.com>
2022-09-22 17:01:17 -0300

    feat: add database class file

--- lib/database.dart
+++ lib/database.dart
@@ -1,3 +1,10 @@
-class Database {
-  
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+
+class MyDatabase {
+  Future<Database> initializeDatabase({required String databaseName}) async {
+    String databasePath = await getDatabasesPath();
+    return openDatabase(join(databasePath, databaseName + '.db'));
+  }
+   
 }
