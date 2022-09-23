d4a0446bd5d33fda42071d60de2fa11ef2ef0cd8
Guilherme Samuel <guisamuel800@gmail.com>
2022-09-23 17:27:46 -0300

    feat: creating v2 of mydatabase class

--- lib/database.dart
+++ lib/database.dart
@@ -1,6 +1,13 @@
 import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
 
+/*
+// Embedding .db files
+import 'dart:typed_data';
+import 'package:flutter/services.dart';
+import 'dart:io';
+*/
+
 class User {
   final String name;
   final String email;
@@ -29,22 +36,70 @@ class User {
 }
 
 class MyDatabase {
-  Database _db;
+  final String name;
 
-  Future<Database> initializeDatabase({required String databaseName}) async {
+  MyDatabase({
+    required this.name
+  });
+  
+  Future<Database> initializeDatabase() async {
     String databasePath = await getDatabasesPath();
-    return openDatabase(join(databasePath, databaseName + '.db'));
+    return openDatabase(
+      join(databasePath, this.name + '.db'),
+      onCreate: (database, version) async {
+        await database.execute(
+          '''
+          CREATE TABLE user(
+            id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+            name VARCHAR(40) NOT NULL,
+            email VARCHAR(20) NOT NULL,
+            phone VARCHAR(16) NOT NULL,
+            username VARCHAR(15) NOT NULL,
+            password VARCHAR(20) NOT NULL
+          );
+          '''
+        );
+      },
+      version: 1
+    );
   }
 
-  Future<int> insertUser(List<User> users) async {
+  Future<int> insertUser(User user) async {
     int result = 0;
-    final Database db = await initializeDatabase();
-    for(User u in users) {
-      result = await db.insert('user', user.toMap());
-    }
-  }
-
-  dispose() {
-    _db.close();
+    final Database database = await initializeDatabase();
+    result = await database.insert('user', user.toMap());
+    return result;
   }
 }
+
+/*
+class MyDatabase {
+  Database _database;
+  final String name;
+
+  MyDatabase({
+    required this.name
+  });
+
+  initDatabase() async {
+    final String fileName = this.name + '.db';
+    _database = await openDatabase('assets/databases/' + fileName);
+    String databasePath = join(await getDatabasesPath(), fileName);
+
+    bool dbExists = await databaseExists(databasePath);
+
+    if(!dbExists) {
+      print('Create a new copy of database from assets!');
+
+      // Copy from assets
+      ByteData data = await rootBundle.load(join('assets/databases', fileName));
+      List<int> bytes = data.buffer.asUint8List(data.offsetInBytes, data.lengthInBytes);
+
+      await File(databasePath).writeAsBytes(bytes, flush: true);
+    }
+
+    // Open the database
+    _db = await openDatabase(databasePath);
+  }  
+}
+*/
