61a5ad07b809c9c202900809d968261cf471188c
Guilherme Samuel <guisamuel800@gmail.com>
2022-10-07 16:46:20 -0300

    style: implement a function that diplays directly a image

--- lib/utility.dart
+++ lib/utility.dart
@@ -2,6 +2,17 @@ import 'dart:typed_data';
 import 'dart:convert';
 
 class Utility {
+  /*
+  static Image imageFromBase64String(String base64String, {required int w, required int h}) {
+    return Image.memory(
+      dataFromBase64String(base64String),
+      width: w,
+      height: h,
+      fit: BoxFit.fill
+    )
+  }
+  */
+  
   static Uint8List dataFromBase64String(String base64String) {
     return base64Decode(base64String);
   }
