4087d2f00cfe54624792a4061e9723990aeb3d0b
Guilherme Samuel <guisamuel800@gmail.com>
2022-10-26 15:17:15 -0300

    feat: create a custom app bar file

---
+++ lib/widgets/my_appbar.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:vitahealth/colors.dart';
+
+class MyAppBar {
+  static AppBar logoAppBar() {
+    return AppBar(
+      title: Image.asset(
+        'assets/images/logo_RGB.png',
+        fit: BoxFit.contain,
+        height: 35.sp
+      ),
+      leading: IconButton(
+        icon: const Icon(
+          Icons.arrow_back_ios
+        ),
+        onPressed: null,
+        color: ProjectColors().title
+      ),
+      centerTitle: true,
+      elevation: 0,
+      backgroundColor: ProjectColors().appBarBackground,
+      foregroundColor: ProjectColors().appBarForeground,
+    );
+  }
+
+  static AppBar textAppBar(String appBarTitle) {
+    return AppBar(
+      title: Text(
+        appBarTitle,
+      ),
+      leading: IconButton(
+        icon: const Icon(
+          Icons.arrow_back_ios
+        ),
+        onPressed: null,
+        color: ProjectColors().title
+      ),
+      centerTitle: true,
+      elevation: 0,
+      backgroundColor: ProjectColors().appBarBackground,
+      foregroundColor: ProjectColors().appBarForeground,
+    );
+  }
+}
