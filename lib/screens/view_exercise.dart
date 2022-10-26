17e6ddc84e307f596de44ac81418aaa3cfeceb68
Guilherme Samuel <guisamuel800@gmail.com>
2022-10-26 17:10:58 -0300

    feat: implement google maps api + exercise button

--- lib/screens/view_exercise.dart
+++ lib/screens/view_exercise.dart
@@ -1,21 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:vitahealth/widgets/button.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class ViewExercise extends StatelessWidget {
-  ViewExercise({
-    Key? key
-  }) : super(key: key);
+class ViewExercise extends StatefulWidget {
+  @override
+  State<ViewExercise> createState() => _ViewExerciseState();
+}
 
+class _ViewExerciseState extends State<ViewExercise> {
+  static const _initialCameraPosition = CameraPosition(
+    target: LatLng(37.773972, -122.431297),
+    zoom: 11.5
+  );
+  
   @override
   Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+    
     return Scaffold(
       body: SafeArea(
         child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 20.sp),
-          child: Center(  
+          padding: EdgeInsets.all(20.sp),
+          child: Center(
             child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: <Widget>[
+                Expanded(
+                  child: GoogleMap(
+                    initialCameraPosition: _initialCameraPosition,
+                    myLocationButtonEnabled: false,
+                    zoomControlsEnabled: false
+                  ),
+                ),
+                SizedBox(height: size.height * 0.1),
                 ExerciseButton(
                   diameter: 90.sp,
                   color: Colors.red,
